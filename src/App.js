9becfe9a618da697a5f0abdfee153e325f218947
fachriansyahmni <fachriansyahmni@gmail.com>
2022-10-12 16:48:49 +0700

    integrasi register api

--- src/App.js
+++ src/App.js
@@ -19,28 +19,33 @@ import MenuKalender from './pages/Guru/MenuKalender/MenuKalender';
 import MateriGuru from './pages/Guru/MateriGuru/MateriGuru';
 import Erapor from './pages/Guru/Erapor/Erapor';
 import PengaturanSiswa from './pages/Siswa/PengaturanSiswa/PengaturanSiswa';
+import axios from 'axios';
 
 function App() {
+  axios.defaults.baseURL = 'http://localhost:8000';
+  // axios.defaults.headers.post['Content-Type'] = 'application/json';
+  // axios.defaults.headers.post['Accept'] = 'application/json';
+  // axios.defaults.withCredentials = true;
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}/>  
-      <Route path='/home' element={<Home/>}/>  
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/tentang' element={<Tentang/>}/>  
-      <Route path='/register' element={<Register/>}/>  
-      <Route path='/contactus' element={<ContactUs/>}/> 
-      <Route path='/loginguru' element={<LoginGuru/>}/>
-      <Route path='/dasboardsiswa' element={<DasboardSiswa/>}/>
-      <Route path='/absensisiswa' element={<AbsensiSiswa/>}/>
-      <Route path='/materisiswa' element={<MateriSiswa/>}/>
-      <Route path='/kalenderakademik' element={<KalenderAkademik/>}/>
-      <Route path='/dasboardguru' element={<DasboardGuru/>}/>
-      <Route path='/pengaturanguru' element={<PengaturanGuru/>}/>
-      <Route path='/kalenderguru' element={<MenuKalender/>}/>
-      <Route path='/materiguru' element={<MateriGuru/>}/>
-      <Route path='/erapor' element={<Erapor/>}/>
-      <Route path='/pengaturansiswa' element={<PengaturanSiswa/>}/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/tentang' element={<Tentang />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/contactus' element={<ContactUs />} />
+        <Route path='/loginguru' element={<LoginGuru />} />
+        <Route path='/dasboardsiswa' element={<DasboardSiswa />} />
+        <Route path='/absensisiswa' element={<AbsensiSiswa />} />
+        <Route path='/materisiswa' element={<MateriSiswa />} />
+        <Route path='/kalenderakademik' element={<KalenderAkademik />} />
+        <Route path='/dasboardguru' element={<DasboardGuru />} />
+        <Route path='/pengaturanguru' element={<PengaturanGuru />} />
+        <Route path='/kalenderguru' element={<MenuKalender />} />
+        <Route path='/materiguru' element={<MateriGuru />} />
+        <Route path='/erapor' element={<Erapor />} />
+        <Route path='/pengaturansiswa' element={<PengaturanSiswa />} />
       </Routes></>
   );
 }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,27 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
-    MDBBtn,
-    MDBContainer,
-    MDBRow,
-    MDBCol,
-    MDBCard,
-    MDBCardBody,
-    MDBCardImage,
-    MDBInput,
-    MDBIcon,
-    MDBCheckbox
-  }
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBInput,
+  MDBIcon,
+  MDBCheckbox
+}
   from 'mdb-react-ui-kit';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import { Container } from 'react-bootstrap';
 import './Register.css';
 import logo from '../../img/Lemon.png'
+import axios from 'axios';
 
 const Register = () => {
+  const [dataRegister, setDataRegister] = useState({
+    nama: '',
+    nisn: '',
+    alamat: '',
+    email: '',
+    password: '',
+    nama_ayah: 'nama ayah', //dummy
+    nama_ibu: 'nama ibu', //dummy
+    tempat: 'bandung', //dummy
+    tgl_lahir: '2022-10-12', //dummy
+    jns_kelamin: 'laki-laki', //dummy
+    agama: 'tebak', //dummy
+    telepon: '0812345678', //dummy
+    kd_pos: '123456', //dummy
+  });
+
+  const handleRegister = () => {
+    console.log(dataRegister);
+    const resp = axios.post('/api/auth/register', dataRegister);
+    resp.then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
   return (
     <div>
-    <NavigationBar/>
-    <MDBContainer fluid>
+      <NavigationBar />
+      <MDBContainer fluid>
         <MDBRow>
           <MDBCol md='3' lg='5' className='order-2 order-lg-1 d-flex flex-column align-items-center mt-5'>
 
@@ -29,52 +55,52 @@ const Register = () => {
             <p className="text-center h7 mb-4">Silahkan isi form dibawah ini untuk registrasi</p>
 
             <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="user me-3" size='lg'/>
-              <MDBInput label='Nama Lengkap' id='form1' type='text' className='w-100 mg1'/>
+              <MDBIcon fas icon="user me-3" size='lg' />
+              <MDBInput label='Nama Lengkap' onChange={(e) => setDataRegister({ ...dataRegister, nama: e.target.value })} id='inputName' type='text' className='w-100 mg1' />
             </div>
 
             <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="user me-3" size='lg'/>
-              <MDBInput label='NISN' id='form1' type='text' className='w-100 mg1'/>
+              <MDBIcon fas icon="user me-3" size='lg' />
+              <MDBInput label='NISN' onChange={(e) => setDataRegister({ ...dataRegister, nisn: e.target.value })} id='inputNisn' type='text' className='w-100 mg1' />
             </div>
 
             <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="user me-3" size='lg'/>
-              <MDBInput label='Alamat' id='form1' type='text' className='w-100 mg1'/>
+              <MDBIcon fas icon="user me-3" size='lg' />
+              <MDBInput label='Alamat' onChange={(e) => setDataRegister({ ...dataRegister, alamat: e.target.value })} id='inputAlamat' type='text' className='w-100 mg1' />
             </div>
 
 
             <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="envelope me-3" size='lg'/>
-              <MDBInput label='Email' id='form2' type='email' className='mg1'/>
+              <MDBIcon fas icon="envelope me-3" size='lg' />
+              <MDBInput label='Email' onChange={(e) => setDataRegister({ ...dataRegister, email: e.target.value })} id='inputEmail' type='email' className='mg1' />
             </div>
 
             <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="lock me-3" size='lg'/>
-              <MDBInput label='Password' id='form3' type='password' className='mg1'/>
+              <MDBIcon fas icon="lock me-3" size='lg' />
+              <MDBInput label='Password' onChange={(e) => setDataRegister({ ...dataRegister, password: e.target.value })} id='inputPassword' type='password' className='mg1' />
             </div>
 
             <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="key me-3" size='lg'/>
-              <MDBInput label='Repeat your password' id='form4' type='password' className='mg1'/>
+              <MDBIcon fas icon="key me-3" size='lg' />
+              <MDBInput label='Repeat your password' id='inputRePassword' type='password' className='mg1' />
             </div>
 
             <div className='mb-4'>
               <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Saya Setuju' />
             </div>
 
-            <MDBBtn className='mb-4' color='warning' style={{color: 'white'}} size='lg'>Register</MDBBtn>
+            <MDBBtn className='mb-4' onClick={handleRegister} color='warning' style={{ color: 'white' }} size='lg'>Register</MDBBtn>
 
           </MDBCol>
 
           <MDBCol md='10' lg='5' className='d-flex align-items-center md1'>
-            <MDBCardImage src={logo} fluid className='md2'/>
+            <MDBCardImage src={logo} fluid className='md2' />
           </MDBCol>
 
         </MDBRow>
 
-  </MDBContainer>
-  </div>
+      </MDBContainer>
+    </div>
   )
 }
 
