2f4ea59c99d55313fde7d34d9279f50c583bbd16
Yukicchi10 <farizsalman27@gmail.com>
2022-10-13 15:15:25 +0700

    Update Register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -13,10 +13,15 @@ import {
 }
   from 'mdb-react-ui-kit';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
-import { Container } from 'react-bootstrap';
+import { Container, FormSelect } from 'react-bootstrap';
 import './Register.css';
 import logo from '../../img/Lemon.png'
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Register = () => {
   const [dataRegister, setDataRegister] = useState({
@@ -25,14 +30,14 @@ const Register = () => {
     alamat: '',
     email: '',
     password: '',
-    nama_ayah: 'nama ayah', //dummy
-    nama_ibu: 'nama ibu', //dummy
-    tempat: 'bandung', //dummy
-    tgl_lahir: '2022-10-12', //dummy
-    jns_kelamin: 'laki-laki', //dummy
-    agama: 'tebak', //dummy
-    telepon: '0812345678', //dummy
-    kd_pos: '123456', //dummy
+    nama_ayah: '', //dummy
+    nama_ibu: '', //dummy
+    tempat: '', //dummy
+    tgl_lahir: '', //dummy
+    jns_kelamin: '', //dummy
+    agama: '', //dummy
+    telepon: '', //dummy
+    kd_pos: '', //dummy
   });
 
   const handleRegister = () => {
@@ -44,6 +49,12 @@ const Register = () => {
       console.log(error);
     });
   }
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
     <div>
       <NavigationBar />
@@ -53,43 +64,121 @@ const Register = () => {
 
             <p className="text-center h1 fw-bold mx-1 mx-md-4 mt-4">Registrasi</p>
             <p className="text-center h7 mb-4">Silahkan isi form dibawah ini untuk registrasi</p>
-
-            <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="user me-3" size='lg' />
-              <MDBInput label='Nama Lengkap' onChange={(e) => setDataRegister({ ...dataRegister, nama: e.target.value })} id='inputName' type='text' className='w-100 mg1' />
-            </div>
-
-            <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="user me-3" size='lg' />
-              <MDBInput label='NISN' onChange={(e) => setDataRegister({ ...dataRegister, nisn: e.target.value })} id='inputNisn' type='text' className='w-100 mg1' />
-            </div>
-
-            <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="user me-3" size='lg' />
-              <MDBInput label='Alamat' onChange={(e) => setDataRegister({ ...dataRegister, alamat: e.target.value })} id='inputAlamat' type='text' className='w-100 mg1' />
-            </div>
+      
 
 
-            <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="envelope me-3" size='lg' />
-              <MDBInput label='Email' onChange={(e) => setDataRegister({ ...dataRegister, email: e.target.value })} id='inputEmail' type='email' className='mg1' />
-            </div>
 
-            <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="lock me-3" size='lg' />
-              <MDBInput label='Password' onChange={(e) => setDataRegister({ ...dataRegister, password: e.target.value })} id='inputPassword' type='password' className='mg1' />
-            </div>
+            <Form.Group className="mb-3">
+        <Form.Label>Nama Lengkap</Form.Label>
+        <Form.Control label='Nama Lengkap' onChange={(e) => setDataRegister({ ...dataRegister, nama: e.target.value })} id='inputName' type='text' className='w-100 mg1' />
+      </Form.Group>
 
-            <div className="d-flex flex-row align-items-center mb-4">
-              <MDBIcon fas icon="key me-3" size='lg' />
-              <MDBInput label='Repeat your password' id='inputRePassword' type='password' className='mg1' />
-            </div>
+            <Form.Group className="mb-3">
+        <Form.Label>NISN</Form.Label>
+        <Form.Control label='NISN' onChange={(e) => setDataRegister({ ...dataRegister, nisn: e.target.value })} id='inputNisn' type='number' className='w-100 mg1' />
+      </Form.Group>
 
-            <div className='mb-4'>
-              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Saya Setuju' />
-            </div>
+            <Form.Group className="mb-3">
+        <Form.Label>Email</Form.Label>
+        <Form.Control label='Email' onChange={(e) => setDataRegister({ ...dataRegister, email: e.target.value })} id='inputEmail' type='email' className='mg1' />
+      </Form.Group>
 
-            <MDBBtn className='mb-4' onClick={handleRegister} color='warning' style={{ color: 'white' }} size='lg'>Register</MDBBtn>
+            <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control onChange={(e) => setDataRegister({ ...dataRegister, password: e.target.value })} id='inputPassword' type='password' className='mg1' />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>Alamat</Form.Label>
+          <Form.Control label='Alamat' onChange={(e) => setDataRegister({ ...dataRegister, alamat: e.target.value })} id='inputAlamat' type='text' className='' />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>Kode Pos</Form.Label>
+          <Form.Control label='Kode Pos' onChange={(e) => setDataRegister({ ...dataRegister, kd_pos: e.target.value })} id='inputPos' type='text' className='' />
+        </Form.Group>
+      </Row>
+
+            <Form.Group className="mb-3">
+        <Form.Label>Nama Ayah</Form.Label>
+        <Form.Control label='Nama Ayah' onChange={(e) => setDataRegister({ ...dataRegister, nama_ayah: e.target.value })} id='inputAyah' type='text' className='w-100 mg1' />
+      </Form.Group>
+
+            <Form.Group className="mb-3">
+        <Form.Label>Nama Ibu</Form.Label>
+        <Form.Control label='Nama Ibu' onChange={(e) => setDataRegister({ ...dataRegister, nama_ibu: e.target.value })} id='inputIbu' type='text' className='w-100 mg1' />
+      </Form.Group>
+
+            <Form.Group className="mb-3">
+        <Form.Label>Nomor Telepon</Form.Label>
+        <Form.Control label='Nomor Telepon' onChange={(e) => setDataRegister({ ...dataRegister, telepon: e.target.value })} id='inputTelepon' type='number' className='w-100 mg1' />
+      </Form.Group>
+
+            <Form.Group className="mb-3">
+        <Form.Label>Tempat Lahir</Form.Label>
+        <Form.Control label='Tempat' onChange={(e) => setDataRegister({ ...dataRegister, tempat: e.target.value })} id='inputTempat' type='text' className='w-100 mg1' />
+      </Form.Group>
+
+            <Form.Group className="mb-3">
+        <Form.Label>Tanggal Lahir</Form.Label>
+        <Form.Control onChange={(e) => setDataRegister({ ...dataRegister, tgl_lahir: e.target.value })} id='inputTgl' type='date' className='w-100 mg1' />
+      </Form.Group>
+
+
+      <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledSelect">Jenis Kelamin</Form.Label>
+          <Form.Select label='Jenis Kelamin' onChange={(e) => setDataRegister({ ...dataRegister, jns_kelamin: e.target.value })} id='inputKelamin' type='select' className='w-100 mg1' >
+            <option disabled selected>- Silahkan Pilih -</option>
+            <option>Laki-Laki</option>
+            <option>Wanita</option>
+            <option>Lainnya</option>
+          </Form.Select>
+        </Form.Group>
+
+
+            <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledSelect">Agama</Form.Label>
+          <Form.Select label='Agama' onChange={(e) => setDataRegister({ ...dataRegister, agama: e.target.value })} id='inputAgama' type='select' className='w-100 mg1' >
+            <option disabled selected>- Silahkan Pilih -</option>
+            <option>Islam</option>
+            <option>Kristen</option>
+            <option>Hindu</option>
+          </Form.Select>
+        </Form.Group>
+            
+
+
+        <Col xs="auto">
+          <Form.Check
+            type="checkbox"
+            id="autoSizingCheck"
+            className="mb-2"
+            label="Saya setuju dan ingin melanjutkan"
+          />
+        </Col>
+        <Col xs="auto">
+
+          <Button className='mb-5' variant="warning" size='lg' onClick={handleShow}>Registrasi</Button>
+          <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Registrasi</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Apakah anda ingin melanjutkan untuk mendaftar?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Kembali
+          </Button>
+          <Button variant="warning" onClick={handleRegister}>
+            Lanjutkan
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
+        </Col>
+
+
 
           </MDBCol>
 
